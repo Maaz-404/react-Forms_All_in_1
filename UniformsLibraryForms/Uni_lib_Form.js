71c9186982d563f1d7ca714999fb906eab512b9e
maaz4 <maazmukhtar966@gmail.com>
2020-07-19 20:36:16 +0500

    Add Semantic UI

--- UniformsLibraryForms/Uni_lib_Form.js
+++ UniformsLibraryForms/Uni_lib_Form.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { AutoForm } from 'uniforms-semantic';
 import GuestSchema from './GuestSchema';
+import 'semantic-ui-css/semantic.min.css'
+
 
 //This form Uses Semantic UI
 
 export default function UniLibraryForm() {
+  
   return (
     <div>
     <h1>_______________________________________</h1>
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import UniLibraryForm from './UniformsLibraryForms/Uni_lib_Form';
 //Style Sheet
 import './style.css';
 
+
 //Render The Forms
 render(
   
